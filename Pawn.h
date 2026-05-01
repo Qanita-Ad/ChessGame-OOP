63c35dd997b5598a839dd9b15a982d3789ef8cb0
Salmana_Adnan <salmanaadnan7@gmail.com>
2026-05-01 17:09:55 +0500

    Inherited Header Files from pieces

--- Pawn.h
+++ Pawn.h
@@ -3,6 +3,10 @@
 #include "pieces.h"
 class pawn :public pieces
 {
-
+public:
+	pawn(char color);
+	~pawn();
+	bool isValidMove(int startRow, int startCol, int endRow, int endCol) override;
+	char getSymbol() override;
 };
 #endif

8f47022b1eda525748dc69c09775102e0143d85c
Salmana_Adnan <salmanaadnan7@gmail.com>
2026-05-01 16:58:08 +0500

    Add files via upload

--- Pieces.h
+++ Pieces.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef PIECES_H
+#define PIECES_H
+#include<iostream>
+#include<string>
+using namespace std;
+class pieces
+{
+protected:
+	char color;
+public:
+	pieces(char color);
+	virtual ~pieces();
+	virtual bool isValidMove(int startRow, int startCol, int endRow, int endCol) = 0;
+	char getColor();
+	virtual char getSymbol() = 0;
+};
+#endif

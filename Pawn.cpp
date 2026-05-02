f02bbaf2d427b902bad6c64f098e0f5827fc0db0
Salmana_Adnan <salmanaadnan7@gmail.com>
2026-05-02 05:57:43 +0500

    Implemented Pawn class with first move logic

--- Pawn.cpp
+++ Pawn.cpp
@@ -1,34 +1,53 @@
 #include "Pieces.h"
 #include "Pawn.h"
-pawn::pawn(char color) : pieces(color) {}
-pawn::~pawn() {}
-bool pawn::isValidMove(int startRow, int startCol, int endRow, int endCol) 
+pawn::pawn(char color) : pieces(color)
 {
-	if (color == 'W')
-	{
-		if (startRow == 6 && endRow == 4 && startCol == endCol) 
-		{
-			return true; 
-		}
-		if (endRow == startRow - 1 && startCol == endCol)
-		{
-			return true; 
-		}
-	} 
-	else
-	{
-		if (startRow == 1 && endRow == 3 && startCol == endCol)
-		{
-			return true; 
-		}
-		if (endRow == startRow + 1 && startCol == endCol)
-		{
-			return true; 
-		}
-	}
-	return false;
+	firstMove = true;
 }
-char pawn::getSymbol() 
+pawn::~pawn() {}
+void pawn::setFirstMove(bool val)
 {
-	return (color == 'W') ? 'P' : 'p';
+    firstMove = val;
+}
+bool pawn::isValidMove(int startRow, int startCol, int endRow, int endCol)
+{
+    if (startRow == endRow && startCol == endCol)
+    {
+        return false;
+    }
+    if (color == 'W')
+    {
+        if (startCol == endCol && endRow == startRow - 1)
+        {
+            return true;
+        }
+        if (firstMove && startCol == endCol && endRow == startRow - 2)
+        {
+            return true;
+        }
+        if (endRow == startRow - 1 && (endCol == startCol - 1 || endCol == startCol + 1))
+        {
+            return true;
+        }
+    }
+    else
+    {
+        if (startCol == endCol && endRow == startRow + 1)
+        {
+            return true;
+        }
+        if (firstMove && startCol == endCol && endRow == startRow + 2)
+        {
+            return true;
+        }
+        if (endRow == startRow + 1 && (endCol == startCol - 1 || endCol == startCol + 1))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+char pawn::getSymbol()
+{
+    return (color == 'W') ? 'P' : 'p';
 }
--- Pawn.h
+++ Pawn.h
@@ -3,9 +3,12 @@
 #include "pieces.h"
 class pawn :public pieces
 {
+private:
+	bool firstMove;
 public:
 	pawn(char color);
 	~pawn();
+	void setFirstMove(bool val);
 	bool isValidMove(int startRow, int startCol, int endRow, int endCol) override;
 	char getSymbol() override;
 };
